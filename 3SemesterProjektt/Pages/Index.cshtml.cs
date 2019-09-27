d66441804be92b25381d0582f94bb1bfaecfaf7e
Julian <juli11c0@edu.eal.dk>
2019-09-27 09:32:52 +0200

    tæller visitor(næsten) på index siden

--- 3SemesterProjektt/Pages/Index.cshtml.cs
+++ 3SemesterProjektt/Pages/Index.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace _3SemesterProjektt.Pages
 {
     public class IndexModel : PageModel
     {
+
+        private readonly ILogger logger;
+
+
+        public string Message;
+        public int Count = 0;
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            this.logger = logger;
+        }
         public void OnGet()
         {
-
+            Count++;
+            Message = $"thank for visiting our awesome site. Visitor number {Count}";
         }
     }
 }
